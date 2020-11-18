8d0fb7116f3671c325784fe1f627eb082f9dc871
Destin Martin <destin@destins-mbp.gateway.sasktel.net>
2020-11-18 02:54:38 -0600

    handling textbox change onSubmit

--- src/New.js
+++ src/New.js
@@ -22,41 +22,43 @@ class New extends React.Component {
 
   handleClick (event){
     event.preventDefault();
-    alert(this.state.text);
-  }
-
-  handleChange(event){
-    event.preventDefault();
+    //alert(event.target.text.value);
     const target= event.target;
-    const value =  target.value;
-    const name = target.name; //this is used by the SetState function to identify the target for the value
+    const value =  target.text.value;
+    const name = target.text.name; //this is used by the SetState function to identify the target for the value
     console.log(value)
      let message = {
       user: this.state.currentUser,
       text: value,
       added: new Date()
     }
-
     const messagesCopy = this.state.messages.slice();
-    messagesCopy.push(message);
+    messagesCopy.push(message);//https://forum.freecodecamp.org/t/this-state-messages-map-is-not-a-function-in-react-redux-help/231361/3
     this.setState({// Stete is immutable, so we don't directly modify it, we replace it.
-      [name]: messagesCopy
+      messages: messagesCopy
     });
+  }
+
+  handleChange(event){
+    event.preventDefault();
+    this.setState({text:event.target.value});
+    
   }
 
 render() {
     return (
     <div>
-      <form>
+      <form onSubmit={this.handleClick}>
         <label>
           New Message
           <input 
+          id="text"
             value={this.state.text} 
             name="messages"  // this name is used by the handleChange event to identify what varable to change. 
             onChange={this.handleChange} />
         </label>
         <br/>
-        <button onClick={this.handleClick}>Send</button>
+        <button>Send</button>
         <MessageList messages={this.state.messages}/>
 
       </form>
